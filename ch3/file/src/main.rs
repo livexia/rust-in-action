9f22a5740e115a3c08bf5ffd293eaa1294436f19
livexia <istillalive@live.cn>
2023-05-06 16:58:59 +0800

    ch3: using enum

--- ch3/file/src/main.rs
+++ ch3/file/src/main.rs
@@ -1,8 +1,15 @@
 use rand::prelude::*;
 
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
+
 #[derive(Debug)]
 struct File {
     name: String,
+    state: FileState,
     data: Vec<u8>,
 }
 
@@ -10,25 +17,35 @@ impl File {
     fn new(name: String) -> Self {
         File {
             name,
+            state: FileState::Closed,
             data: Vec::new(),
         }
     }
 
-    fn open(self) -> Result<Self, String> {
+    fn open(mut self) -> Result<Self, String> {
         if one_in(10_000) {
-            return Err("Permission denied".to_string());
+            return Err("Permission denied!".to_string());
         }
+        if let FileState::Open = self.state {
+            return Err("File already open!".to_string());
+        }
+
+        self.state = FileState::Open;
         Ok(self)
     }
 
-    fn close(self) -> Result<Self, String> {
+    fn close(mut self) -> Result<Self, String> {
         if one_in(100_000) {
             return Err("Interrupted by signal!".to_string());
         }
+        self.state = FileState::Closed;
         Ok(self)
     }
 
     fn read(&self, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        if let FileState::Closed = self.state {
+            return Err("File must be open for reading first!".to_string());
+        }
         let mut temp = self.data.clone();
         let read_length = temp.len();
 
@@ -52,6 +69,13 @@ fn main() -> Result<(), String> {
     let file = File::new("2.txt".to_string());
 
     let mut buffer = vec![];
+
+    {
+        // test file open
+        if file.read(&mut buffer).is_err() {
+            println!("Error checking is working");
+        }
+    }
 
     let mut file = file.open()?;
     file.write(&[114, 117, 115, 116, 31])?;
