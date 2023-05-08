ab43c3d601efa253a26ad08387099d42d673bc07
livexia <istillalive@live.cn>
2023-05-08 20:49:13 +0800

    ch3: write more doc, and modify struct visibility

--- ch3/file/src/main.rs
+++ ch3/file/src/main.rs
@@ -10,49 +10,57 @@ pub enum FileState {
     Closed,
 }
 
+/// Represents a "file"
 #[derive(Debug)]
-struct File {
-    name: String,
-    state: FileState,
+pub struct File {
+    pub name: String,
+    pub state: FileState,
     data: Vec<u8>,
 }
 
-trait Read {
+/// Trait for read
+pub trait Read {
+    /// Reads file data to save_to.
     fn read(&self, save_to: &mut Vec<u8>) -> Result<usize, String>;
 }
 
-trait Write {
+/// Trait for Write
+pub trait Write {
+    /// Write input to file data.
     fn write(&mut self, input: &[u8]) -> Result<usize, String>;
 }
 
 impl File {
-    fn new(name: String) -> Self {
+    /// New files are assumed to be empty, but a name is required.
+    pub fn new(name: String) -> Self {
         File {
             name,
             state: FileState::Closed,
             data: Vec::new(),
         }
     }
+}
 
-    fn open(mut self) -> Result<Self, String> {
-        if one_in(10_000) {
-            return Err("Permission denied!".to_string());
-        }
-        if let FileState::Open = self.state {
-            return Err("File already open!".to_string());
-        }
-
-        self.state = FileState::Open;
-        Ok(self)
+/// Opens a file, set file state to FileState::Open
+pub fn open(mut f: File) -> Result<File, String> {
+    if one_in(10_000) {
+        return Err("Permission denied!".to_string());
+    }
+    if let FileState::Open = f.state {
+        return Err("File already open!".to_string());
     }
 
-    fn close(mut self) -> Result<Self, String> {
-        if one_in(100_000) {
-            return Err("Interrupted by signal!".to_string());
-        }
-        self.state = FileState::Closed;
-        Ok(self)
+    f.state = FileState::Open;
+    Ok(f)
+}
+
+/// Closes a file, set file state to FileState::Closed
+pub fn close(mut f: File) -> Result<File, String> {
+    if one_in(100_000) {
+        return Err("Interrupted by signal!".to_string());
     }
+    f.state = FileState::Closed;
+    Ok(f)
 }
 
 impl Display for FileState {
@@ -91,10 +99,12 @@ impl Write for File {
     }
 }
 
+/// Function for random panic, only useful in testing
 fn one_in(denominator: u32) -> bool {
     thread_rng().gen_ratio(1, denominator)
 }
 
+/// Main funtion for this program and testing.
 #[allow(clippy::read_zero_byte_vec)]
 fn main() -> Result<(), String> {
     // for _ in 0..1_000_000 {
@@ -109,7 +119,7 @@ fn main() -> Result<(), String> {
         }
     }
 
-    let mut file = file.open()?;
+    let mut file = open(file)?;
     file.write(&[114, 117, 115, 116, 31])?;
     let file_length = file.read(&mut buffer)?;
     let text = String::from_utf8_lossy(&buffer);
@@ -117,7 +127,7 @@ fn main() -> Result<(), String> {
     println!("{}", file); // testing Display
     println!("{} is {} byte long", file.name, file_length);
     println!("{}", text);
-    file.close()?;
+    close(file)?;
     // }
 
     Ok(())
