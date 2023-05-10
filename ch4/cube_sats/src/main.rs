a0621e38bfba3631a8e9aa00afbbbb11fc042750
livexia <istillalive@live.cn>
2023-05-10 17:34:06 +0800

    ch4: adding message

--- ch4/cube_sats/src/main.rs
+++ ch4/cube_sats/src/main.rs
@@ -3,20 +3,43 @@ enum StatusMessage {
     Ok,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct CubeSat {
     id: u64,
+    mailbox: MailBox,
 }
+
+type Message = String;
+
+#[derive(Debug, Default)]
+struct MailBox {
+    messages: Vec<Message>,
+}
+
+struct GroundStation;
 
 impl CubeSat {
     fn new(id: u64) -> Self {
-        Self { id }
+        Self {
+            id,
+            ..Default::default()
+        }
+    }
+
+    fn recv(&mut self) -> Option<String> {
+        self.mailbox.messages.pop()
     }
 }
 
 fn check_status(sat: &CubeSat) -> StatusMessage {
     let _ = sat.id;
     StatusMessage::Ok
+}
+
+impl GroundStation {
+    fn send(&self, to: &mut CubeSat, msg: Message) {
+        to.mailbox.messages.push(msg);
+    }
 }
 
 fn main() {
@@ -34,4 +57,15 @@ fn main() {
     let b_status = check_status(&sat_b);
     let c_status = check_status(&sat_c);
     println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+
+    let base = GroundStation {};
+    let mut sat_a = CubeSat::new(0);
+    println!("t0: {:?}", sat_a);
+
+    base.send(&mut sat_a, "Hello".to_string());
+    println!("t1: {:?}", sat_a);
+
+    let msg = sat_a.recv();
+    println!("t2: {:?}", sat_a);
+    println!("sat_a received: {:?}", msg);
 }
