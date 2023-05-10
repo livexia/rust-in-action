f4b2fe86253c2feed2dbcfdbc60022b18ad2ee88
livexia <istillalive@live.cn>
2023-05-10 17:20:44 +0800

    ch4: using reference to resolve ownership issue

--- ch4/cube_sats/src/main.rs
+++ ch4/cube_sats/src/main.rs
@@ -14,8 +14,8 @@ impl CubeSat {
     }
 }
 
-fn check_status(sat: CubeSat) -> StatusMessage {
-    let _ = sat;
+fn check_status(sat: &CubeSat) -> StatusMessage {
+    let _ = sat.id;
     StatusMessage::Ok
 }
 
@@ -24,14 +24,14 @@ fn main() {
     let sat_b = CubeSat::new(1);
     let sat_c = CubeSat::new(2);
 
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
+    let a_status = check_status(&sat_a);
+    let b_status = check_status(&sat_b);
+    let c_status = check_status(&sat_c);
     println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 
     // waiting
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
+    let a_status = check_status(&sat_a);
+    let b_status = check_status(&sat_b);
+    let c_status = check_status(&sat_c);
     println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 }
