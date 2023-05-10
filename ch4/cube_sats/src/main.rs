aea9d9c8276fe4aa96923e8483e24ab7f2534fd7
livexia <istillalive@live.cn>
2023-05-10 18:02:24 +0800

    ch4: adding intersection

--- ch4/cube_sats/src/main.rs
+++ ch4/cube_sats/src/main.rs
@@ -3,13 +3,16 @@ enum StatusMessage {
     Ok,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct CubeSat {
     id: u64,
-    mailbox: MailBox,
 }
 
-type Message = String;
+#[derive(Debug)]
+struct Message {
+    to: u64,
+    content: String,
+}
 
 #[derive(Debug, Default)]
 struct MailBox {
@@ -18,16 +21,32 @@ struct MailBox {
 
 struct GroundStation;
 
-impl CubeSat {
-    fn new(id: u64) -> Self {
-        Self {
-            id,
-            ..Default::default()
-        }
+impl MailBox {
+    fn new() -> Self {
+        Self::default()
     }
 
-    fn recv(&mut self) -> Option<String> {
-        self.mailbox.messages.pop()
+    fn post(&mut self, msg: Message) {
+        self.messages.push(msg);
+    }
+
+    fn deliver(&mut self, recipient: &CubeSat) -> Option<Message> {
+        for i in 0..self.messages.len() {
+            if self.messages[i].to == recipient.id {
+                return Some(self.messages.remove(i));
+            }
+        }
+        None
+    }
+}
+
+impl CubeSat {
+    fn new(id: u64) -> Self {
+        Self { id }
+    }
+
+    fn recv(&self, mailbox: &mut MailBox) -> Option<Message> {
+        mailbox.deliver(self)
     }
 }
 
@@ -37,9 +56,27 @@ fn check_status(sat: &CubeSat) -> StatusMessage {
 }
 
 impl GroundStation {
-    fn send(&self, to: &mut CubeSat, msg: Message) {
-        to.mailbox.messages.push(msg);
+    fn send(&self, mailbox: &mut MailBox, msg: Message) {
+        mailbox.post(msg);
     }
+
+    fn connect(&self, id: u64) -> CubeSat {
+        CubeSat::new(id)
+    }
+}
+
+impl Message {
+    fn new(to: &CubeSat, content: String) -> Self {
+        Self { to: to.id, content }
+    }
+
+    fn read(self) -> String {
+        self.content
+    }
+}
+
+fn fetch_ids() -> Vec<u64> {
+    vec![0, 1, 2, 3]
 }
 
 fn main() {
@@ -58,14 +95,26 @@ fn main() {
     let c_status = check_status(&sat_c);
     println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 
+    let mut mailbox = MailBox::new();
     let base = GroundStation {};
-    let mut sat_a = CubeSat::new(0);
-    println!("t0: {:?}", sat_a);
 
-    base.send(&mut sat_a, "Hello".to_string());
-    println!("t1: {:?}", sat_a);
+    // Send
+    let sat_ids = fetch_ids();
 
-    let msg = sat_a.recv();
-    println!("t2: {:?}", sat_a);
-    println!("sat_a received: {:?}", msg);
+    for id in sat_ids {
+        let sat = base.connect(id);
+        let msg = Message::new(&sat, format!("Hello: {id}"));
+        base.send(&mut mailbox, msg)
+    }
+
+    // Recv
+    let sat_ids = fetch_ids();
+
+    for id in sat_ids {
+        let sat = base.connect(id);
+        let msg = sat.recv(&mut mailbox);
+        if let Some(msg) = msg {
+            println!("{:?}: {}", sat, msg.read());
+        }
+    }
 }
