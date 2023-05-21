9d0e73ca1567572d996c35cbaa4cec03dab133ff
livexia <istillalive@live.cn>
2023-05-21 17:38:35 +0800

    ch6: unsafe decoding string

--- ch6/ch6-pointers/src/main.rs
+++ ch6/ch6-pointers/src/main.rs
@@ -1,7 +1,7 @@
 use std::println;
 
-static B: [u8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-static C: [u8; 11] = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 0];
+static B: [u8; 10] = [99, 97, 114, 114, 121, 116, 111, 119, 109, 108];
+static C: [u8; 11] = [116, 104, 97, 110, 107, 115, 102, 105, 115, 104, 0];
 
 fn main() {
     let a = 42;
@@ -24,4 +24,39 @@ fn main() {
     e[10] = 100;
     println!("After change e[10] to 100: {:?}", e);
     println!("C is still unchanged: {:?}", C);
+
+    let b_string = decoding();
+    // After b_string and c_cow out of scope program is crashing,
+    // because with String now b_string has the ownership of B
+    // when b_string droped, B is also droped?
+    //
+    println!("b_string: {b_string}");
+    drop(b_string);
+    println!("After drop b_string, program crashing");
+    println!("&B -> {:p}", &B);
+}
+
+use std::borrow::Cow;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+fn decoding() -> String {
+    let a = 42;
+
+    let b: String;
+
+    let c: Cow<str>;
+
+    // SAFETY: this is unsafe, will crasing program
+    // this is bad, should be avoid
+    unsafe {
+        let b_ptr = &B as *const u8 as *mut u8;
+
+        b = String::from_raw_parts(b_ptr, 10, 10);
+
+        let c_ptr = &C as *const u8 as *const c_char;
+
+        c = CStr::from_ptr(c_ptr).to_string_lossy();
+    }
+    println!("a: {a}, b: {b}, c: {c}");
+    b
 }
