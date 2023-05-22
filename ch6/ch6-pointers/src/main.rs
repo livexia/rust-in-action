61e693b4e21e874acfcd847a4b136ee15e9dc9bb
livexia <istillalive@live.cn>
2023-05-22 15:35:49 +0800

    ch6: i64 address

--- ch6/ch6-pointers/src/main.rs
+++ ch6/ch6-pointers/src/main.rs
@@ -2,7 +2,13 @@ static B: [u8; 10] = [99, 97, 114, 114, 121, 116, 111, 119, 109, 108];
 static C: [u8; 11] = [116, 104, 97, 110, 107, 115, 102, 105, 115, 104, 0];
 
 fn main() {
-    let a = 42;
+    let a: i64 = 42;
+
+    let a_ptr = &a as *const i64;
+    let a_addr: usize = unsafe { std::mem::transmute(a_ptr) };
+
+    println!("a: {} ({:p}...0x{:x})", a, a_ptr, a_addr + 7);
+
     let b = &B;
     let c = &C;
     let p_b = u64::from_str_radix(format!("{b:p}").strip_prefix("0x").unwrap(), 16).unwrap();
