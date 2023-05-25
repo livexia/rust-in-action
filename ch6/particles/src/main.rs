c4bff559ee1a1acfaac884bda0a0581c5f0ebaf8
livexia <istillalive@live.cn>
2023-05-25 22:06:54 +0800

    ch6: writing own allocator

--- ch6/particles/src/main.rs
+++ ch6/particles/src/main.rs
@@ -2,6 +2,37 @@ extern crate piston_window;
 
 use piston_window::*;
 
+use std::alloc::{GlobalAlloc, Layout, System};
+
+use std::eprintln;
+use std::time::Instant;
+
+#[global_allocator]
+static ALLOCATOR: ReportingAllocator = ReportingAllocator;
+
+struct ReportingAllocator;
+
+unsafe impl GlobalAlloc for ReportingAllocator {
+    // why return *mut u8
+    // see: https://old.reddit.com/r/rust/comments/y3t5cf/why_doesnt_globalallocalloc_return_a_usize_pointer/
+    // and: https://users.rust-lang.org/t/why-does-alloc-return-u8/55290
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let start = Instant::now();
+        let ptr = System.alloc(layout);
+        let end = Instant::now();
+        let time_taken = end - start;
+        let bytes_requested = layout.size();
+
+        eprintln!("{}\t{}", bytes_requested, time_taken.as_nanos());
+
+        ptr
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        System.dealloc(ptr, layout)
+    }
+}
+
 fn main() {
     let (width, height) = (1280, 960);
     let mut window: PistonWindow = WindowSettings::new("Particles", [width, height])
