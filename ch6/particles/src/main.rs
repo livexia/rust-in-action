075b67f7118620d91736ea9a6d9fc5b48a1fa906
livexia <istillalive@live.cn>
2023-05-25 22:28:40 +0800

    ch6: impl Particle

--- ch6/particles/src/main.rs
+++ ch6/particles/src/main.rs
@@ -1,10 +1,11 @@
 extern crate piston_window;
 
+use piston_window::math::{add, mul_scalar, Vec2d};
 use piston_window::*;
+use rand::prelude::*;
 
 use std::alloc::{GlobalAlloc, Layout, System};
 
-use std::eprintln;
 use std::time::Instant;
 
 #[global_allocator]
@@ -30,6 +31,56 @@ unsafe impl GlobalAlloc for ReportingAllocator {
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         System.dealloc(ptr, layout)
+    }
+}
+
+struct World {
+    curent_turn: u64,
+    particles: Vec<Box<Particle>>, // using box to use allocator
+    height: f64,
+    width: f64,
+    rng: ThreadRng,
+}
+
+struct Particle {
+    height: f64,
+    width: f64,
+    position: Vec2d<f64>,
+    velocity: Vec2d<f64>,
+    acceleration: Vec2d<f64>,
+    color: [f32; 4],
+}
+
+impl Particle {
+    fn new(world: &World) -> Self {
+        let mut rng = thread_rng();
+        let x = rng.gen_range(0.0..=world.width);
+        let y = world.height;
+        let x_velocity = 0.0;
+        let y_velocity = rng.gen_range(-2.0..0.0);
+        let x_acceleration = 0.0;
+        let y_acceleration = rng.gen_range(0.0..0.15);
+
+        Self {
+            height: 4.0,
+            width: 4.0,
+            position: [x, y].into(),
+            velocity: [x_velocity, y_velocity].into(),
+            acceleration: [x_acceleration, y_acceleration].into(),
+            color: [1.0, 1.0, 1.0, 0.99],
+        }
+    }
+
+    fn update(&mut self) {
+        // move Particle to next position
+        self.velocity = add(self.velocity, self.acceleration);
+        self.position = add(self.position, self.velocity);
+
+        // slow down the acceleration
+        self.acceleration = mul_scalar(self.acceleration, 0.7);
+
+        // makes the particle mor transparent over time
+        self.color[3] *= 0.95;
     }
 }
 
