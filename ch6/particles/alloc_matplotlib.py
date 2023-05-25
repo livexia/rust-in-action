dea72b12aee134f9ef2c901197fa3fd65c2e5015
livexia <istillalive@live.cn>
2023-05-26 01:59:25 +0800

    ch6: fix: matplotlib script with right parse

--- ch6/particles/alloc_matplotlib.py
+++ ch6/particles/alloc_matplotlib.py
@@ -7,15 +7,18 @@ data = [line.split("\t", 1) for line in read_data]
 
 print(f"data count: {len(data)}")
 
-sizes = [line[0] for line in data]
-times = [line[1] for line in data]
+sizes = [int(line[0]) for line in data]
+times = [int(line[1]) for line in data]
 
 print(f"size count: {len(sizes)}, time count: {len(times)}")
+print(f"min size: {min(sizes)}, max size: {max(sizes)}")
+print(f"min time: {min(times)}, max time: {max(times)}")
 
 plt.plot(sizes, times, "r.")
 plt.xscale("log", base=2)
 plt.yscale("log")
 plt.xlabel("Allocation size (bytes)")
 plt.ylabel("Allocation duration (ns)")
-plt.axis([1, 100000, 0, 10000])
+plt.axis([1, 100000, 1, 10000])
+plt.minorticks_on()
 plt.show()
