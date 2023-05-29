c878e6388fd372bfb633be5450ca6b24711aaf3b
livexia <istillalive@live.cn>
2023-05-29 13:57:13 +0800

    memscan: trying mach_vm_write

--- ch6/memscan/src/main.rs
+++ ch6/memscan/src/main.rs
@@ -1,6 +1,6 @@
 use libc::{c_int, pid_t};
 use mach2::message::mach_msg_type_number_t;
-use mach2::vm::{mach_vm_read_overwrite, mach_vm_region};
+use mach2::vm::{mach_vm_read_overwrite, mach_vm_region, mach_vm_write};
 use mach2::vm_region::{
     vm_region_basic_info_data_64_t, vm_region_basic_info_data_t, vm_region_info_t,
     VM_REGION_BASIC_INFO,
@@ -21,26 +21,16 @@ macro_rules! err {
 
 pub type Result<T> = result::Result<T, Box<dyn Error>>;
 
-fn main() -> Result<()> {
-    let task = task_for_pid(70793).unwrap();
-    println!("task: {task}");
-    let base_addr = 0x16d0ce534;
-    let size = 4;
-    let raw_data = vm_read(task, base_addr, size).unwrap();
-    let data: u32 = u32::from_le_bytes(raw_data[..4].try_into().unwrap());
-    println!("raw data: {:?}", raw_data);
-    println!("data at 0x{base_addr:0x} is {data}");
-
-    let result = search_value(task, base_addr, &12999usize.to_le_bytes())?;
-    Ok(())
-}
-
-pub fn search_value(task: mach_port_name_t, address: usize, pattern: &[u8]) -> Result<Vec<usize>> {
+pub fn search_value(
+    task: mach_port_name_t,
+    region_address: usize,
+    region_size: usize,
+    pattern: &[u8],
+) -> Result<Vec<usize>> {
     let mut result = vec![];
     let data_size = pattern.len();
-    let (address, size) = vm_region(task, address as _)?;
-    for addr in (address..address + size).step_by(data_size) {
-        let raw_data = vm_read(task, addr, data_size).unwrap();
+    for addr in (region_address..region_address + region_size).step_by(data_size) {
+        let raw_data = vm_read_overwrite(task, addr, data_size)?;
         if raw_data == pattern {
             result.push(addr);
             println!("Found value {raw_data:?} at: 0x{addr:0x}");
@@ -52,7 +42,7 @@ pub fn search_value(task: mach_port_name_t, address: usize, pattern: &[u8]) -> R
 
 pub fn vm_region(task: mach_port_name_t, mut address: mach_vm_address_t) -> Result<(usize, usize)> {
     let mut size: mach_vm_size_t = unsafe { std::mem::zeroed() };
-    let mut info: vm_region_basic_info_data_t = unsafe { std::mem::zeroed() };
+    let mut info: vm_region_basic_info_data_64_t = unsafe { std::mem::zeroed() };
     let mut info_cnt =
         std::mem::size_of::<vm_region_basic_info_data_64_t>() as mach_msg_type_number_t;
     let mut object_name = 0;
@@ -62,7 +52,7 @@ pub fn vm_region(task: mach_port_name_t, mut address: mach_vm_address_t) -> Resu
             &mut address,
             &mut size,
             VM_REGION_BASIC_INFO,
-            &mut info as *mut vm_region_basic_info_data_t as vm_region_info_t,
+            &mut info as *mut vm_region_basic_info_data_64_t as vm_region_info_t,
             &mut info_cnt,
             &mut object_name,
         )
@@ -74,7 +64,7 @@ pub fn vm_region(task: mach_port_name_t, mut address: mach_vm_address_t) -> Resu
     Ok((address as usize, size as usize))
 }
 
-pub fn vm_read(task: mach_port_name_t, address: usize, size: usize) -> Result<Vec<u8>> {
+pub fn vm_read_overwrite(task: mach_port_name_t, address: usize, size: usize) -> Result<Vec<u8>> {
     let mut buf = vec![0u8; size];
     let mut read_len = 0;
 
@@ -91,6 +81,14 @@ pub fn vm_read(task: mach_port_name_t, address: usize, size: usize) -> Result<Ve
         return Err(Box::new(io::Error::last_os_error()));
     }
     Ok(buf)
+}
+
+pub fn vm_write(task: mach_port_name_t, address: usize, buffer: &[u8], size: usize) -> Result<()> {
+    let result = unsafe { mach_vm_write(task, address as _, buffer.as_ptr() as _, size as _) };
+    if result != KERN_SUCCESS {
+        return Err(Box::new(io::Error::last_os_error()));
+    }
+    Ok(())
 }
 
 //
@@ -111,4 +109,30 @@ pub fn task_for_pid(pid: Pid) -> Result<mach_port_name_t> {
         }
     }
     Ok(task)
+}
+
+fn main() -> Result<()> {
+    let pid = 85555;
+    let task = task_for_pid(pid)?;
+    println!("pid: {pid}, task: {task}");
+
+    let mut address = 0;
+    let mut size = 0;
+    while address < usize::MAX {
+        (address, size) = vm_region(task, (address + size) as _)?;
+        println!("address: 0x{address:0x} size: {size}");
+        if let Ok(result) = search_value(task, address, size, &900usize.to_le_bytes()) {
+        } else {
+            println!("Err");
+        };
+    }
+
+    vm_write(
+        task,
+        0x16d0ce540,
+        &[255, 255, 255, 255, 255, 255, 255, 255],
+        8,
+    )?;
+
+    Ok(())
 }
