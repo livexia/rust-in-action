ec07d937b7801e0ef24caff1ee39ce74d80c31e6
livexia <istillalive@live.cn>
2023-06-01 15:19:53 +0800

    actionkv: finishing cli program for now

--- ch7/actionkv/src/akv_mem.rs
+++ ch7/actionkv/src/akv_mem.rs
@@ -1,3 +1,54 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+use libactionkv::ActionKV;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// FILE for ActionKV
+    #[arg(value_name = "FILE")]
+    fname: PathBuf,
+
+    /// Operation commands
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Retrieves the value at key from the store
+    Get { key: String },
+    /// Adds a key-value pair to the store
+    Insert { key: String, value: String },
+    /// Removes a key-value pair from the store
+    Delete { key: String },
+    /// Replaces an old value with a new one
+    Update { key: String, value: String },
+}
+
 fn main() {
-    println!("Hello,World!");
+    let args = Cli::parse();
+
+    let path = args.fname;
+    let mut store = ActionKV::open(&path).expect("unable to open file");
+    store.load().expect("unable to load data");
+
+    match &args.command {
+        Commands::Get { key } => {
+            println!("Get {key}: {:?}", store.get(key));
+        }
+        Commands::Insert { key, value } => {
+            println!("Insert {key} {value}");
+            store.insert(key, value);
+        }
+        Commands::Delete { key } => {
+            println!("Delete {key}");
+            store.delete(key);
+        }
+        Commands::Update { key, value } => {
+            println!("Update {key} {value}");
+            store.update(key, value);
+        }
+    }
 }
