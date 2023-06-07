87ac7f4c6a63f0c06906e4cb65b9d6d1e0540741
livexia <istillalive@live.cn>
2023-06-07 21:25:08 +0800

    ch8: build http get from tcp

---
+++ ch8/http_from_tcp/src/main.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+use std::io::prelude::*;
+use std::net::{SocketAddr, TcpStream, ToSocketAddrs};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let host = "httpstat.us";
+    let port = "80";
+    let addr: Vec<SocketAddr> = format!("{host}:{port}").to_socket_addrs()?.collect();
+
+    let mut stream = TcpStream::connect(&addr[..])?;
+    stream.write_all(b"GET / HTTP/1.0")?;
+    stream.write_all(b"\r\n")?;
+
+    stream.write_all(b"Host: httpstat.us")?;
+    stream.write_all(b"\r\n\r\n")?;
+
+    let mut response = String::new();
+    stream.read_to_string(&mut response)?;
+    println!("Response: {response}");
+    Ok(())
+}
