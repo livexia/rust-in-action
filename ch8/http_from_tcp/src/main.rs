972c61e7bb970c08503eff091214322dc7c7791c
livexia <istillalive@live.cn>
2023-06-07 21:39:53 +0800

    ch8: adding more header

--- ch8/http_from_tcp/src/main.rs
+++ ch8/http_from_tcp/src/main.rs
@@ -8,14 +8,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let addr: Vec<SocketAddr> = format!("{host}:{port}").to_socket_addrs()?.collect();
 
     let mut stream = TcpStream::connect(&addr[..])?;
+    // 1.1 will hang
     stream.write_all(b"GET / HTTP/1.0")?;
     stream.write_all(b"\r\n")?;
 
-    stream.write_all(b"Host: httpstat.us")?;
+    stream.write_all(b"Host: ")?;
+    stream.write(host.as_bytes())?;
     stream.write_all(b"\r\n\r\n")?;
 
     let mut response = String::new();
     stream.read_to_string(&mut response)?;
     println!("Response: {response}");
+
     Ok(())
 }
