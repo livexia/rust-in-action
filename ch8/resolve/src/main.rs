9d4fa37a3860fe7ee7d210c08eb36195f58adbe8
livexia <istillalive@live.cn>
2023-06-08 16:50:51 +0800

    ch8: resolve: manual construct dns query message then using udpsocket to send it

--- ch8/resolve/src/main.rs
+++ ch8/resolve/src/main.rs
@@ -1,34 +1,75 @@
 use std::error::Error;
-use std::net::SocketAddr;
+use std::net::{SocketAddr, UdpSocket};
+use std::time::Duration;
 
 use clap::{arg, Command};
 use trust_dns_client::client::{Client, SyncClient};
-use trust_dns_client::op::DnsResponse;
+use trust_dns_client::op::{DnsResponse, Message, MessageType, OpCode, Query};
 use trust_dns_client::rr::{DNSClass, Name, RData, Record, RecordType};
+use trust_dns_client::serialize::binary::{BinEncodable, BinEncoder};
 use trust_dns_client::udp::UdpClientConnection;
 
 // see: https://docs.rs/trust-dns-client/latest/trust_dns_client/
-fn trust_dns_client_udp(domain_name: &str, dns_server: SocketAddr) -> Result<(), Box<dyn Error>> {
+fn trust_dns_client_udp(
+    domain_name: &str,
+    dns_server: SocketAddr,
+) -> Result<Vec<String>, Box<dyn Error>> {
     let conn = UdpClientConnection::new(dns_server)?;
     let client = SyncClient::new(conn);
-    let name: Name = domain_name.parse()?;
-    let response: DnsResponse = client.query(&name, DNSClass::IN, RecordType::A)?;
+    let domain_name: Name = domain_name.parse()?;
+    let response: DnsResponse = client.query(&domain_name, DNSClass::IN, RecordType::A)?;
 
     let answers: &[Record] = response.answers();
 
+    let mut records = vec![];
     for answer in answers {
         if let Some(RData::A(ref ip)) = answer.data() {
-            println!("{}", ip.to_string());
+            records.push(ip.to_string());
         }
     }
-    Ok(())
+
+    Ok(records)
 }
 
 fn trust_dns_client_msg_udp(
     domain_name: &str,
     dns_server: SocketAddr,
-) -> Result<(), Box<dyn Error>> {
-    Ok(())
+) -> Result<Vec<String>, Box<dyn Error>> {
+    let domain_name: Name = domain_name.parse()?;
+    let mut request_as_bytes: Vec<u8> = Vec::with_capacity(512);
+    let mut response_as_bytes: Vec<u8> = vec![0; 512];
+
+    let mut msg = Message::new();
+    msg.set_id(rand::random::<u16>())
+        .set_message_type(MessageType::Query)
+        .add_query(Query::query(domain_name, RecordType::A))
+        .set_op_code(OpCode::Query)
+        .set_recursion_desired(true);
+
+    let mut encoder = BinEncoder::new(&mut request_as_bytes);
+    msg.emit(&mut encoder).unwrap();
+
+    let localhost = UdpSocket::bind("0.0.0.0:0")?;
+    let timeout = Duration::from_secs(3);
+    localhost.set_read_timeout(Some(timeout))?;
+    localhost.set_nonblocking(false)?;
+
+    let _amt = localhost.send_to(&request_as_bytes, dns_server)?;
+
+    let (_amt, _remote) = localhost.recv_from(&mut response_as_bytes)?;
+
+    let dns_message = Message::from_vec(&response_as_bytes)?;
+
+    let answers: &[Record] = dns_message.answers();
+
+    let mut records = vec![];
+    for answer in answers {
+        if let Some(RData::A(ref ip)) = answer.data() {
+            records.push(ip.to_string());
+        }
+    }
+
+    Ok(records)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -45,8 +86,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         (Some(domain_name), Some(dns_server)) => {
             match format!("{dns_server}:53").parse::<SocketAddr>() {
                 Ok(dns_server) => {
-                    trust_dns_client_udp(domain_name, dns_server)?;
-                    trust_dns_client_msg_udp(domain_name, dns_server)?;
+                    let r1 = trust_dns_client_udp(domain_name, dns_server)?;
+                    let r2 = trust_dns_client_msg_udp(domain_name, dns_server)?;
+                    assert_eq!(r1, r2);
+                    for ip in r1 {
+                        println!("{ip}");
+                    }
                     Ok(())
                 }
                 Err(err) => {
