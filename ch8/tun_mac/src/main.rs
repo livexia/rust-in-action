a5c4f13614bb4040a1cdba34d0e2f594c23b8b36
livexia <istillalive@live.cn>
2023-06-11 01:25:17 +0800

    ch8: trying workout how tun works

--- ch8/tun_mac/src/main.rs
+++ ch8/tun_mac/src/main.rs
@@ -12,30 +12,10 @@ fn main() {
 
     let mut dev = tun::create(&config).unwrap();
     let mut buf = [0; 4096];
-    println!("broadcast: {:?}", dev.address());
+    println!("address: {:?}", dev.address());
 
     loop {
         let amount = dev.read(&mut buf).unwrap();
         println!("{:?}", &buf[0..amount]);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::net::{TcpListener, TcpStream};
-
-    fn handel_client(stream: TcpStream) {
-        println!("{stream:?}");
-    }
-
-    #[test]
-    fn it_works() -> std::io::Result<()> {
-        let listener = TcpListener::bind("192.168.42.100:80")?;
-
-        for stream in listener.incoming() {
-            handel_client(stream?);
-        }
-
-        Ok(())
     }
 }
--- ch8/tun_mac/src/tun_test.rs
+++ ch8/tun_mac/src/tun_test.rs
@@ -1,32 +1,36 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
-use std::thread;
+use std::thread::{self, sleep};
+use std::time::Duration;
 
-fn handle_client(stream: TcpStream) {
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     println!("{stream:?}");
+    let mut buf = [0; 4096];
+    let size = stream.read(&mut buf)?;
+
+    println!("recv data: \n{:?}", &buf[..size]);
+    Ok(())
 }
 
 fn listen() -> std::io::Result<()> {
-    let listener = TcpListener::bind("192.168.42.100:8080")?;
-    println!("listen on: 192.168.42.100:8080");
+    let url = "0.0.0.0:8080";
+    let listener = TcpListener::bind(url)?;
+    println!("listen on: {url}");
 
     for (i, stream) in listener.incoming().enumerate() {
         println!("incoming stream: {i}");
-        handle_client(stream?);
+        handle_client(stream?)?;
     }
 
     Ok(())
 }
 
 fn send() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("192.168.41.100:8080")?;
     println!("Try to send");
+    let mut stream = TcpStream::connect("192.168.42.100:8080")?;
 
-    stream.write(&[1])?;
+    stream.write(&[1; 8])?;
 
-    let mut buf = [0; 128];
-    stream.read(&mut buf)?;
-    println!("{:?}", buf);
     Ok(())
 }
 
@@ -34,6 +38,7 @@ fn main() -> std::io::Result<()> {
     let _handle = thread::spawn(|| {
         listen().unwrap();
     });
-    send()?;
+    println!("- {:?}", send());
+    sleep(Duration::from_secs(30));
     Ok(())
 }
