fe4c2997c6b97af7f593472204560fbd500fbba1
livexia <istillalive@live.cn>
2023-06-10 22:49:10 +0800

    ch8: using rust_tun to create utun on mac

---
+++ ch8/tun_mac/src/main.rs
@@ -0,0 +1,21 @@
+use std::io::Read;
+
+use tun::Device;
+
+// see: https://lib.rs/crates/tun
+fn main() {
+    let mut config = tun::Configuration::default();
+    config
+        .address((192, 168, 42, 100))
+        .netmask((255, 255, 255, 0))
+        .up();
+
+    let mut dev = tun::create(&config).unwrap();
+    let mut buf = [0; 4096];
+    println!("broadcast: {:?}", dev.address());
+
+    loop {
+        let amount = dev.read(&mut buf).unwrap();
+        println!("{:?}", &buf[0..amount]);
+    }
+}
