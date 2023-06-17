4f582ecb88661f95ad8e320c11c14f0e65407308
livexia <istillalive@live.cn>
2023-06-17 17:52:34 +0800

    ch9 clock: get time with corresponding format

--- ch9/clock/src/main.rs
+++ ch9/clock/src/main.rs
@@ -2,6 +2,23 @@ use chrono::Local;
 use clap::{Arg, Command};
 use time::OffsetDateTime;
 
+struct Clock;
+
+impl Clock {
+    fn get(format: &str) -> String {
+        match format {
+            "timestamp" => Local::now().timestamp().to_string(),
+            "rfc2822" => Local::now().to_rfc2822(),
+            "rfc3339" => Local::now().to_rfc3339(),
+            _ => unreachable!(),
+        }
+    }
+
+    fn set(_format: &str, _datetime: &str) -> ! {
+        unimplemented!("set is yet to be implemented")
+    }
+}
+
 fn main() {
     let matches = Command::new("Clock")
         .version("0.1")
@@ -22,18 +39,20 @@ fn main() {
         .get_matches();
 
     let format = matches.get_one::<String>("format").unwrap();
-    dbg!(&format);
 
     match matches.subcommand() {
         Some(("set", set_matches)) => {
-            dbg!(&set_matches);
+            let datetime = set_matches.get_one::<String>("datetime").unwrap();
+            Clock::set(format, datetime);
         }
         Some(_) | None => {
-            let chrono_local_now = Local::now();
-            let time_local_now =
-                OffsetDateTime::now_local().expect("time crate unable read time from local");
-            println!("chrono crate local now: {}", chrono_local_now);
-            println!("time crate local now: {:?}", time_local_now);
+            let now = Clock::get(format);
+            println!("{now}");
         }
     }
+    let chrono_local_now = Local::now();
+    let time_local_now =
+        OffsetDateTime::now_local().expect("time crate unable read time from local");
+    eprintln!("chrono crate local now: {}", chrono_local_now);
+    eprintln!("time crate local now: {:?}", time_local_now);
 }
